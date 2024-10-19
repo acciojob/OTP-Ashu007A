af8752b8ca61cb82153569e85694fd73346648a4
Ashish Kumar Ram <74091038+Ashu007A@users.noreply.github.com>
2024-10-19 11:37:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+document.addEventListener("DOMContentLoaded", function() {
+    const codes = document.querySelectorAll('.code');
+    
+    codes.forEach((code, idx) => {
+        code.addEventListener('input', (e) => {
+            if (e.target.value.length > 0) {
+                if (idx < codes.length - 1) {
+                    codes[idx + 1].focus();
+                }
+            }
+        });
+
+        code.addEventListener('keydown', (e) => {
+            if (e.key === 'Backspace' && !e.target.value) {
+                if (idx > 0) {
+                    codes[idx - 1].focus();
+                }
+            }
+        });
+    });
+});
